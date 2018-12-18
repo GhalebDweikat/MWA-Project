904479e455856c03de9448f18a434ffe8207239e
GhalebDweikat <GhalebDweikat@gmail.com>
2018-12-18 00:03:14 -0600

    Update route

--- SPA/OnlineShopping/src/app/auth.service.ts
+++ SPA/OnlineShopping/src/app/auth.service.ts
@@ -1,23 +1,42 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { map } from 'rxjs/operators';
+import * as moment from "moment";
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable()
 export class AuthService {
 
   constructor(private http: HttpClient) {
+
   }
 
   login(email:string, password:string ) {
-    return this.http.post<any>('/v1/users/authenticate', {email: email, password: password})
-      .pipe(map(user => {
-        if(user && user.token) {
-          //save somewhere
-          return user;
-        }
-        return null;
-      }));
+    return this.http.post<any>('/api/login', {email: email, password: password});
+      //.do(res => this.setSession)
+  }
+
+  private setSession(authResult) {
+    const expiresAt = moment().add(1,'hour');
+
+    localStorage.setItem('id_token', authResult.data.token);
+    localStorage.setItem("expires_at", JSON.stringify(expiresAt.valueOf()) );
+  }
+
+  logout() {
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
+  }
+
+  public isLoggedIn() {
+    return moment().isBefore(this.getExpiration());
+  }
+
+  isLoggedOut() {
+    return !this.isLoggedIn();
+  }
+
+  getExpiration() {
+    const expiration = localStorage.getItem("expires_at");
+    const expiresAt = JSON.parse(expiration);
+    return moment(expiresAt);
   }
 }
